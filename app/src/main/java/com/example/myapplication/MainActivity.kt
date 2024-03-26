514eda30828f6457dda7b9c8f3802497dd90a450
al416753 <al416753@uji.es>
2024-03-26 10:11:53 +0100

    Funciona View Model :D

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
 import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MiView {
     private lateinit var binding: ActivityMainBinding
+    private val miViewModel: MiViewModel by viewModels()
     private var num=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +18,31 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener{apretar(it)}
     }
-    fun apretar(view: View){
+    override fun apretar(view: View){
         num++
         binding.textView.setText(num)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        miViewModel.view = this
+    }
+    override fun onPause() {
+        super.onPause()
+        miViewModel.view = null
+    }
+}
+
+interface MiView {
+    fun apretar(view:View)
+}
+
+class MiViewModel: ViewModel() {
+    var view: MiView? = null
+        set(value) {
+            field = value
+            updateView()
+        }
+    private fun updateView() = view?.apply {
     }
 }
